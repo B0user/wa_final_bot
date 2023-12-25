48cb9e6d57e7f01acd7c7496127178cabb90747a
ArmanJumashev <124795505+ArmanJumashev@users.noreply.github.com>
2023-12-25 11:48:31 +0600

    last i hope

--- index.js
+++ index.js
@@ -48,35 +48,35 @@ client.on('message', async message => {
     const msgContent = message.body.toLowerCase().trim();
     const currentState = userStates[senderId] || {};
 
-    if (msgContent.includes === 'здравствуйте' || msgContent.includes === 'добрый') {
+    if (msgContent.includes('здравствуйте') || msgContent.includes('добрый') || msgContent.includes('доброе') || msgContent.includes('салам')) {
         // Установка начального шага, ожидание ввода пользователя
         currentState.step = '0';
-        await client.sendMessage(senderId, "Здравствуйте! Вас рада приветствовать стоматология ИДЕАЛ!\n Наш сайт: ideal-stom.kz\n Наш инстаграм: @idealstomkrg\n С Вами на связи робот, просим Вас сообщить, что Вас интересует: \n 1. Запись на лечение зуба/ов\n 2. Запись на чистку зубов, лечение десен\n 3. Запись на удаление зуба/ов\n 4. Запись на консультацию по имплантам\n 5. Запись на   консультацию по брекетам (исправлению прикуса)\n 6. Запись на   консультацию по протезированию\n 7. Хочу задать вопрос\n 8. Прошу перенести мою запись\n 9. Прошу отменить мою  запись");
+        await client.sendMessage(senderId, "Здравствуйте! Вас рада приветствовать стоматология ИДЕАЛ!\n Наш сайт: ideal-stom.kz\n Наш инстаграм: @idealstom.krg\n \n С Вами на связи робот, просим Вас сообщить, что Вас интересует: \n 1. Запись на лечение зуба/ов\n 2. Запись на чистку зубов, лечение десен\n 3. Запись на удаление зуба/ов\n 4. Запись на консультацию по имплантам\n 5. Запись на консультацию по брекетам (исправлению прикуса)\n 6. Запись на консультацию по протезированию\n 7. Хочу задать вопрос\n 8. Прошу перенести мою запись\n 9. Прошу отменить мою запись\n Введите ответ цифрой от 1 до 9");
     }
     switch (currentState.step) {
         case '0':
             // Пользователь только начал взаимодействовать, бот ожидает выбора опции
-            if (msgContent.includes("1") ||  msgContent.includes("2") || msgContent.includes("3")) {
-                await client.sendMessage(senderId, 'У вас есть страховка? Ответьте "да" или "нет"');
+            if (msgContent === "1" ||  msgContent === "2" || msgContent === "3") {
+                await client.sendMessage(senderId, 'У Вас есть страховка Медикер (на работе выдавали карту Медикер)? Ответьте "да" или "нет"');
                 currentState.step = 'mediker';
             } 
-            else if(msgContent.includes("4")){
-                await client.sendMessage(senderId, 'У вас есть 3Д рентген снимок Ваших челюстей?');
+            else if(msgContent === "4"){
+                await client.sendMessage(senderId, 'У Вас есть 3Д рентген снимок Ваших челюстей?');
                 currentState.step = 'rentgen';
             }
-            else if(msgContent.includes("5") || msgContent.includes("6")){
+            else if(msgContent === "5" || msgContent === "6"){
                 await client.sendMessage(senderId, "Напишите Ваше ФИО и дату рождения полностью, администратор предложит Вам время записи");
                 currentState.step = 'fio_i_dr';
             }
-            else if(msgContent.includes("7")){
-                await client.sendMessage(senderId, "Напишите ваш вопрос, Вам ответит администратор в ближайшее рабочее время");
+            else if(msgContent === "7"){
+                await client.sendMessage(senderId, "Напишите Ваш вопрос, Вам ответит администратор в ближайшее рабочее время");
                 currentState.step = 'vopros';
             }
-            else if(msgContent.includes("8")){
+            else if(msgContent === "8"){
                 await client.sendMessage(senderId, "Напишите Ваше ФИО, администратор предложит Вам время записи");
                 currentState.step = 'fio_perenos';
             }
-            else if(msgContent.includes("9")){
+            else if(msgContent === "9"){
                 await client.sendMessage(senderId, "Напишите Ваше ФИО, отменим Вашу запись");
                 currentState.step = 'fio_otmena';
             }
@@ -86,7 +86,7 @@ client.on('message', async message => {
             }
             break;
         case 'mediker':
-            if(msgContent.includes("да") || msgContent.includes("нет")){
+            if(msgContent === "да" || msgContent === "нет"){
                 await client.sendMessage(senderId, 'Напишите Ваше ФИО и дату рождения полностью, администратор предложит Вам время записи');
                 currentState.step = 'fio_i_dr';
             }
@@ -95,12 +95,13 @@ client.on('message', async message => {
             }
             break;
         case 'rentgen':
-            if(msgContent.includes("да")){
+            if(msgContent === "да"){
                 await client.sendMessage(senderId, 'Напишите Ваше ФИО и дату рождения полностью, администратор предложит Вам время записи');
                 currentState.step = 'fio_i_dr';
             }
-            else if(msgContent.includes("нет")){
+            else if(msgContent === "нет"){
                 await client.sendMessage(senderId, 'Снимок 3Д можно сделать по адресу: ул.Ерубаева, 58 (Академия красоты, 4 кабинет) либо: ул.Костенко, 10 (клиника Дантист)');
+                await client.sendMessage(senderId, 'Напишите Ваше ФИО и дату рождения полностью, администратор предложит Вам время записи');
                 currentState.step = 'net_3d_rentgena';
             }
             else{
